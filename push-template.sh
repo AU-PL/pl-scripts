e8fc26d43b5b24f86642f5c8583184815238bf74
Harley Eades <harley.eades@gmail.com>
2021-01-11 13:22:51 -0500

    Updating the template files.

---
+++ push-template.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+TEMPLATE=~/teaching/PL/student-repo-template
+STUDENT_REPOS=~/teaching/PL/student-repos
+
+# Main loop.
+for i in $(ls $TEMPLATE)
+do
+    # A [s]tudent [r]epo.
+    SR=${STUDENT_REPOS}/$i
+        
+    # At this point the homework is ready to be copied.
+    cp -vR $TEMPLATE/* ${SR}/
+    
+    # Commit the homework to Git.
+    cd $SR
+    git add .
+    git commit -a -m "Updating the template files."
+    git fetch
+    git rebase
+    git push
+done
